c06cca54567ce19165e035806086563018f60afe
HelenaTennant <helena@example.com>
2026-05-31 19:08:13 +0200

    Add frontend practice projects

---
+++ project-01-no-code-landing/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const award = document.querySelector(".award__images");
+
+award.addEventListener('click', () => {
+    document.querySelector(".award").remove();
+});
+
+// Video Selection 1
+
+const btnClickVideo = document.getElementById("content__btn2");
+
+const video = document.getElementById("content__videos");
+function openVideo() {
+   video.play();
+}
+
+btnClickVideo.addEventListener('click', openVideo);
